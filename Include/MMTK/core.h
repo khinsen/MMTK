40ee4bdfc74702a90621b40d3bd1c4c4a1f66941
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2006-06-22 17:08:27 +0200

    Fixed Py_ssize_t conflict with Scientific

--- Include/MMTK/core.h
+++ Include/MMTK/core.h
@@ -2,7 +2,7 @@
  * General include file for MMTK C modules.
  *
  * Written by Konrad Hinsen
- * last revision: 2006-5-11
+ * last revision: 2006-6-22
  */
 
 #ifndef MMTK_H
@@ -12,6 +12,8 @@
 
 /* Provide Py_ssize_t for Python < 2.5 */
 #if PY_VERSION_HEX < 0x02050000
+#if !defined(PY_SSIZE_T_COMPATIBILITY)
+#define PY_SSIZE_T_COMPATIBILITY
 typedef int Py_ssize_t;
 #define PY_SSIZE_T_MAX INT_MAX
 #define PY_SSIZE_T_MIN INT_MIN
@@ -21,6 +23,11 @@ typedef PyObject *(*ssizeargfunc)(PyObject *, Py_ssize_t);
 typedef PyObject *(*ssizessizeargfunc)(PyObject *, Py_ssize_t, Py_ssize_t);
 typedef int(*ssizeobjargproc)(PyObject *, Py_ssize_t, PyObject *);
 typedef int(*ssizessizeobjargproc)(PyObject *, Py_ssize_t, Py_ssize_t, PyObject *);
+typedef Py_ssize_t (*readbufferproc)(PyObject *, Py_ssize_t, void **);
+typedef Py_ssize_t (*writebufferproc)(PyObject *, Py_ssize_t, void **);
+typedef Py_ssize_t (*segcountproc)(PyObject *, Py_ssize_t *);
+typedef Py_ssize_t (*charbufferproc)(PyObject *, Py_ssize_t, char **);
+#endif
 #endif
 
 /* MinGW doesn't have this */
