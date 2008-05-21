7758bac9b4012f8853ca2a26d41864c548481c89
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2008-05-21 17:13:59 +0200

    Skeleton: fix force field imports
    Depending on which submodules of MMTK were imported by the application program, some force field
    objects used in a trajectory could not be imported, causing an infinite loop when attempting
    to open the trajectory.
    
    At this occasion, the remaining tabs in Skeleton.py were replaced by spaces.

--- MMTK/Skeleton.py
+++ MMTK/Skeleton.py
@@ -17,17 +17,17 @@ import copy, sys, types
 class A:
 
     def __init__(self, name, index, type = None):
-	self.name = name
-	self.index = index
-	self.type = type
+        self.name = name
+        self.index = index
+        self.type = type
 
     def make(self, info, conf = None):
-	atom =  MMTK.Atom(self.type, name = self.name)
+        atom =  MMTK.Atom(self.type, name = self.name)
         self.assignIndex(atom, info, conf)
-	return atom
+        return atom
 
     def assignIndex(self, atom, info, conf):
-	atom.setArray(None, [self.index])
+        atom.setArray(None, [self.index])
         info[self.index] = atom
         if conf is not None:
             atom.setPosition(MMTK.Vector(conf[self.index]))
@@ -38,23 +38,23 @@ class A:
 class Composite:
 
     def __init__(self, name, list, type = None, **kwargs):
-	self.name = name
-	self.list = list
-	self.type = type
-	self.kwargs = kwargs
+        self.name = name
+        self.list = list
+        self.type = type
+        self.kwargs = kwargs
 
     def make(self, info, conf = None):
-	object = self._class(self.type, name=self.name)
-	for sub in self.list:
-	    sub.assignIndex(getattr(object, sub.name), info, conf)
+        object = self._class(self.type, name=self.name)
+        for sub in self.list:
+            sub.assignIndex(getattr(object, sub.name), info, conf)
         if self.kwargs.has_key('dc'):
             for a1, a2, d in self.kwargs['dc']:
                 object.addDistanceConstraint(info[a1], info[a2], d)
-	return object
+        return object
 
     def assignIndex(self, object, info, conf):
-	for sub in self.list:
-	    sub.assignIndex(getattr(object, sub.name), info, conf)
+        for sub in self.list:
+            sub.assignIndex(getattr(object, sub.name), info, conf)
 
 class G(Composite):
     pass
@@ -78,29 +78,29 @@ class S(Composite):
 
     def make(self, info, conf = None):
         import MMTK.Proteins
-	n_residues = len(self.type)/3
-	residues = map(lambda i, s = self.type: s[3*i:3*i+3],
+        n_residues = len(self.type)/3
+        residues = map(lambda i, s = self.type: s[3*i:3*i+3],
                        range(n_residues))
-	self.kwargs['name'] = self.name
-	chain = apply(MMTK.Proteins.PeptideChain, (residues,), self.kwargs)
-	for i in range(len(self.list)):
-	    self.list[i].assignIndex(chain[i], info, conf)
+        self.kwargs['name'] = self.name
+        chain = apply(MMTK.Proteins.PeptideChain, (residues,), self.kwargs)
+        for i in range(len(self.list)):
+            self.list[i].assignIndex(chain[i], info, conf)
             chain[i].name = self.list[i].name
-	return chain
+        return chain
 
 class N(Composite):
 
     def make(self, info, conf = None):
         import MMTK.NucleicAcids
-	n_residues = len(self.type)/3
-	residues = map(lambda i, s = self.type: s[3*i:3*i+3].strip(),
+        n_residues = len(self.type)/3
+        residues = map(lambda i, s = self.type: s[3*i:3*i+3].strip(),
                        range(n_residues))
-	self.kwargs['name'] = self.name
-	chain = apply(MMTK.NucleicAcids.NucleotideChain, (residues,),
+        self.kwargs['name'] = self.name
+        chain = apply(MMTK.NucleicAcids.NucleotideChain, (residues,),
                       self.kwargs)
-	for i in range(len(self.list)):
-	    self.list[i].assignIndex(chain[i], info, conf)
-	return chain
+        for i in range(len(self.list)):
+            self.list[i].assignIndex(chain[i], info, conf)
+        return chain
 
 #
 # Collections and universes
@@ -108,20 +108,20 @@ class N(Composite):
 class c:
 
     def __init__(self, creation, objects):
-	self.creation = creation
-	self.objects = objects
+        self.creation = creation
+        self.objects = objects
 
     def make(self, info, conf = None):
         collection = _evalString(self.creation)
         attr = None
-	for o in self.objects:
+        for o in self.objects:
             if isinstance(o, str):
                 attr = o
             elif attr:
                 setattr(collection, attr, o.make(info, conf))
             else:
                 collection.addObject(o.make(info, conf))
-	return collection
+        return collection
 
 #
 # Objects constructed from a list of other objects (e.g. proteins)
@@ -129,16 +129,16 @@ class c:
 class l:
 
     def __init__(self, class_name, name, objects):
-	self.class_name = class_name
-	self.objects = objects
-	self.name = name
+        self.class_name = class_name
+        self.objects = objects
+        self.name = name
 
     def make(self, info, conf = None):
         import MMTK.Proteins
         classes = {'Protein': MMTK.Proteins.Protein}
-	return classes[self.class_name] \
-	       (map(lambda o, i=info, c=conf: o.make(i, c), self.objects),
-		name = self.name)
+        return classes[self.class_name] \
+               (map(lambda o, i=info, c=conf: o.make(i, c), self.objects),
+                name = self.name)
 
 #
 # Objects without subobjects
@@ -146,10 +146,10 @@ class l:
 class o:
 
     def __init__(self, creation):
-	self.creation = creation
+        self.creation = creation
 
     def make(self, info, conf = None):
-	return _evalString(self.creation)
+        return _evalString(self.creation)
 
 #
 # Evaluate description string
@@ -161,7 +161,8 @@ class o:
 def _evalString(description):
     local = {}
     namespace = copy.copy(vars(MMTK))
-    imported = []
+    namespace['MMTK'] = MMTK
+    imported = ['MMTK', 'MMTK.ForceFields']
     done = False
     while not done:
         try:
