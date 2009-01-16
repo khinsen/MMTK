531ac9bdb40bccbb60352bee65d4c21e9cc97dfb
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2009-01-16 16:10:59 +0100

    Revision of MMTK.Dynamics - Converted docstrings to epytext - Replaced Numeric by Scientific.N - Import of other MMTK modules made explicit - Removed unused imports

--- MMTK/Dynamics.py
+++ MMTK/Dynamics.py
@@ -1,16 +1,18 @@
 # This module implements MD integrators
 #
 # Written by Konrad Hinsen
-# last revision: 2006-11-27
+# last revision: 2009-1-16
 #
 
-"""See also the Example:MolecularDynamics example applications.
+"""
+Molecular Dynamics integrators
 """
 
-from Scientific import N as Numeric
-import Environment, Features, ThreadManager, Trajectory, Units
+__docformat__ = 'epytext'
+
+from MMTK import Environment, Features, ThreadManager, Trajectory, Units
 import MMTK_dynamics
-import operator
+from Scientific import N
 
 try:
     import threading
@@ -67,59 +69,61 @@ class Integrator(Trajectory.TrajectoryGenerator):
 #
 class VelocityVerletIntegrator(Integrator):
 
-    """Velocity-Verlet molecular dynamics integrator
+    """
+    Velocity-Verlet molecular dynamics integrator
 
     The integrator can handle fixed atoms, distance constraints,
     a thermostat, and a barostat, as well as any combination.
     It is fully thread-safe.
 
-    Constructor: VelocityVerletIntegrator(|universe|, |**options|)
-
-    Arguments:
-
-    |universe| -- the universe on which the integrator acts
-
-    |options| -- keyword options:
-
-      * steps: the number of integration steps (default is 100)
-      * delta_t: the time step (default is 1 fs)
-      * actions: a list of actions to be executed periodically (default is
-                 none)
-      * threads: the number of threads to use in energy evaluation
-                 (default set by MMTK_ENERGY_THREADS)
-      * background: if true, the integration is executed as a separate thread
-                    (default: 0)
-      * mpi_communicator: an MPI communicator object, or None, meaning
-                          no parallelization (default: None)
-
     The integration is started by calling the integrator object.
-    All the keyword options listed above can be specified either when
-    creating the integrator or when calling it.
+    All the keyword options (see documnentation of __init__) can be
+    specified either when creating the integrator or when calling it.
 
     The following data categories and variables are available for
     output:
 
-    - category "time": time
+     - category "time": time
 
-    - category "configuration": configuration and box size (for
-      periodic universes)
+     - category "configuration": configuration and box size (for
+       periodic universes)
 
-    - category "velocities": atomic velocities
+     - category "velocities": atomic velocities
 
-    - category "gradients": energy gradients for each atom
+     - category "gradients": energy gradients for each atom
 
-    - category "energy": potential and kinetic energy, plus
-      extended-system energy terms if a thermostat and/or barostat
-      are used
+     - category "energy": potential and kinetic energy, plus
+       extended-system energy terms if a thermostat and/or barostat
+       are used
 
-    - category "thermodynamic": temperature, volume (if a barostat
-      is used) and pressure
+     - category "thermodynamic": temperature, volume (if a barostat
+       is used) and pressure
 
-    - category "auxiliary": extended-system coordinates if a thermostat
-      and/or barostat are used
+     - category "auxiliary": extended-system coordinates if a thermostat
+       and/or barostat are used
     """
 
     def __init__(self, universe, **options):
+        """
+        @param universe: the universe on which the integrator acts
+        @type universe: L{MMTK.Universe}
+        @keyword steps: the number of integration steps (default is 100)
+        @type steps: C{int}
+        @keyword delta_t: the time step (default is 1 fs)
+        @type delta_t: C{float}
+        @keyword actions: a list of actions to be executed periodically
+                          (default is none)
+        @type actions: C{list}
+        @keyword threads: the number of threads to use in energy evaluation
+                          (default set by MMTK_ENERGY_THREADS)
+        @type threads: C{int}
+        @keyword background: if True, the integration is executed as a
+                             separate thread (default: False)
+        @type background: C{bool}
+        @keyword mpi_communicator: an MPI communicator object, or C{None},
+                                   meaning no parallelization (default: C{None})
+        @type mpi_communicator: C{Scientific.MPI.MPICommunicator}
+        """
         Integrator.__init__(self, universe, options)
         self.features = [Features.FixedParticleFeature,
                          Features.NoseThermostatFeature,
@@ -127,6 +131,10 @@ class VelocityVerletIntegrator(Integrator):
                          Features.DistanceConstraintsFeature]
 
     def __call__(self, **options):
+        """
+        Run the integrator. The keyword options are the same as described
+        under L{__init__}.
+        """
         self.setCallOptions(options)
         used_features = Features.checkFeatures(self, self.universe)
         configuration = self.universe.configuration()
@@ -153,8 +161,8 @@ class VelocityVerletIntegrator(Integrator):
             t_parameters = thermostat.parameters
             t_coordinates = thermostat.coordinates
         else:
-            t_parameters = Numeric.zeros((0,), Numeric.Float)
-            t_coordinates = Numeric.zeros((2,), Numeric.Float)
+            t_parameters = N.zeros((0,), N.Float)
+            t_coordinates = N.zeros((2,), N.Float)
         if Features.AndersenBarostatFeature in used_features:
             if self.universe.cellVolume() is None:
                 raise ValueError("Barostat requires finite volume universe")
@@ -169,8 +177,8 @@ class VelocityVerletIntegrator(Integrator):
             b_parameters = barostat.parameters
             b_coordinates = barostat.coordinates
         else:
-            b_parameters = Numeric.zeros((0,), Numeric.Float)
-            b_coordinates = Numeric.zeros((1,), Numeric.Float)
+            b_parameters = N.zeros((0,), N.Float)
+            b_coordinates = N.zeros((1,), N.Float)
 
         args = (self.universe,
                 configuration.array, velocities.array,
@@ -194,153 +202,150 @@ class VelocityVerletIntegrator(Integrator):
 #
 class VelocityScaler(Trajectory.TrajectoryAction):
 
-    """Periodic velocity scaling action
+    """
+    Periodic velocity scaling action
 
     A VelocityScaler object is used in the action list of
     a VelocityVerletIntegrator. It rescales all atomic velocities
     by a common factor to make the temperature of the system equal
     to a predefined value.
-
-    Constructor: VelocityScaler(|temperature|, |temperature_window|=0.,
-                                |first|=0, |last|=None, |skip|=1)
-
-    Arguments:
-
-    |temperature| -- the temperature value to which the velocities should
-                     be scaled
-
-    |temperature_window| -- the deviation from the ideal temperature that
-                            is tolerated in either direction before rescaling
-                            takes place
-
-    |first| -- the number of the first step at which the action is executed
-
-    |last| -- the number of the last step at which the action is executed.
-              A value of 'None' indicates that the action should be
-              executed indefinitely.
-
-    |skip| -- the number of steps to skip between two applications of the
-              action
     """
 
     def __init__(self, temperature, window=0., first=0, last=None, skip=1):
+        """
+        @param temperature: the temperature value to which the velocities
+                            should be scaled
+        @type temperature: C{float}
+        @param window: the deviation from the ideal temperature that
+                       is tolerated in either direction before rescaling
+                       takes place
+        @type window: C{float}
+        @param first: the number of the first step at which the action
+                      is executed
+        @type first: C{int}
+        @param last: the number of the first step at which the action
+                     is no longer executed. A value of C{None} indicates
+                     that the action should be executed indefinitely.
+        @type last: C{int}
+        @param skip: the number of steps to skip between two applications
+                     of the action
+        @type skip: C{int}
+        """
         Trajectory.TrajectoryAction.__init__(self, first, last, skip)
-        self.parameters = Numeric.array([temperature, window], Numeric.Float)
+        self.parameters = N.array([temperature, window], N.Float)
         self.Cfunction = MMTK_dynamics.scaleVelocities
 
 class Heater(Trajectory.TrajectoryAction):
 
-    """Periodic heating action
+    """
+    Periodic heating action
 
     A Heater object us used in the action list of a VelocityVerletIntegrator.
-    It scales the velocities to a temperature that increases with time.
-
-    Constructor: Heater(|temperature1|, |temperature2|, |gradient|,
-                        |first|=0, |last|=None, |skip|=1)
-
-    Arguments:
-
-    |temperature1| -- the temperature value to which the velocities should
-                      be scaled initially
-
-    |temperature2| -- the final temperature value to which the velocities
-                      should be scaled
-
-    |gradient| -- the temperature gradient (in K/ps)
-
-    |first| -- the number of the first step at which the action is executed
-
-    |last| -- the number of the last step at which the action is executed.
-              A value of 'None' indicates that the action should be
-              executed indefinitely.
-
-    |skip| -- the number of steps to skip between two applications of the
-              action
+    It scales the velocities to a temperature that increases over time.
     """
 
     def __init__(self, temp1, temp2, gradient, first=0, last=None, skip=1):
+        """
+        @param temp1: the temperature value to which the velocities
+                      should be scaled initially
+        @type temp1: C{float}
+        @param temp2: the final temperature value to which the velocities
+                      should be scaled
+        @type temp2: C{float}
+        @param gradient: the temperature gradient (in K/ps)
+        @type gradient: C{float}
+        @param first: the number of the first step at which the action
+                      is executed
+        @type first: C{int}
+        @param last: the number of the first step at which the action
+                     is no longer executed. A value of C{None} indicates
+                     that the action should be executed indefinitely.
+        @type last: C{int}
+        @param skip: the number of steps to skip between two applications
+                     of the action
+        @type skip: C{int}
+        """
         Trajectory.TrajectoryAction.__init__(self, first, last, skip)
-        self.parameters = Numeric.array([temp1, temp2, gradient],
-                                        Numeric.Float)
+        self.parameters = N.array([temp1, temp2, gradient], N.Float)
         self.Cfunction = MMTK_dynamics.heat
 
 class BarostatReset(Trajectory.TrajectoryAction):
 
-    """Barostat reset action
+    """
+    Barostat reset action
 
     A BarostatReset object is used in the action list of a
     VelocityVerletIntegrator. It resets the barostat coordinate
     to zero.
-
-    Constructor: BarostatReset(|first|=0, |last|=None, |skip|=1)
-
-    Arguments:
-
-    |first| -- the number of the first step at which the action is executed
-
-    |last| -- the number of the last step at which the action is executed.
-              A value of 'None' indicates that the action should be
-              executed indefinitely.
-
-    |skip| -- the number of steps to skip between two applications of the
-              action
     """
 
     def __init__(self, first=0, last=None, skip=1):
+        """
+        @param first: the number of the first step at which the action
+                      is executed
+        @type first: C{int}
+        @param last: the number of the first step at which the action
+                     is no longer executed. A value of C{None} indicates
+                     that the action should be executed indefinitely.
+        @type last: C{int}
+        @param skip: the number of steps to skip between two applications
+                     of the action
+        @type skip: C{int}
+        """
         Trajectory.TrajectoryAction.__init__(self, first, last, skip)
-        self.parameters = Numeric.zeros((0,), Numeric.Float)
+        self.parameters = N.zeros((0,), N.Float)
         self.Cfunction = MMTK_dynamics.resetBarostat
 
 class TranslationRemover(Trajectory.TrajectoryAction):
 
-    """Action that eliminates global translation
+    """
+    Action that eliminates global translation
 
     A TranslationRemover object is used in the action list of a
     VelocityVerletIntegrator. It subtracts the total velocity
-    from the system from each atomic velocity.
-
-    Constructor: TranslationRemover(|first|=0, |last|=None, |skip|=1)
-
-    Arguments:
-
-    |first| -- the number of the first step at which the action is executed
-
-    |last| -- the number of the last step at which the action is executed.
-              A value of 'None' indicates that the action should be
-              executed indefinitely.
-
-    |skip| -- the number of steps to skip between two applications of the
-              action
+    of the system from each atomic velocity.
     """
 
     def __init__(self, first=0, last=None, skip=1):
+        """
+        @param first: the number of the first step at which the action
+                      is executed
+        @type first: C{int}
+        @param last: the number of the first step at which the action
+                     is no longer executed. A value of C{None} indicates
+                     that the action should be executed indefinitely.
+        @type last: C{int}
+        @param skip: the number of steps to skip between two applications
+                     of the action
+        @type skip: C{int}
+        """
         Trajectory.TrajectoryAction.__init__(self, first, last, skip)
         self.parameters = None
         self.Cfunction = MMTK_dynamics.removeTranslation
 
 class RotationRemover(Trajectory.TrajectoryAction):
 
-    """Action that eliminates global rotation
+    """
+    Action that eliminates global rotation
 
     A RotationRemover object is used in the action list of a
     VelocityVerletIntegrator. It adjusts the atomic velocities
     such that the total angular momentum is zero.
-
-    Constructor: RotationRemover(|first|=0, |last|=None, |skip|=1)
-
-    Arguments:
-
-    |first| -- the number of the first step at which the action is executed
-
-    |last| -- the number of the last step at which the action is executed.
-              A value of 'None' indicates that the action should be
-              executed indefinitely.
-
-    |skip| -- the number of steps to skip between two applications of the
-              action
     """
 
     def __init__(self, first=0, last=None, skip=1):
+        """
+        @param first: the number of the first step at which the action
+                      is executed
+        @type first: C{int}
+        @param last: the number of the first step at which the action
+                     is no longer executed. A value of C{None} indicates
+                     that the action should be executed indefinitely.
+        @type last: C{int}
+        @param skip: the number of steps to skip between two applications
+                     of the action
+        @type skip: C{int}
+        """
         Trajectory.TrajectoryAction.__init__(self, first, last, skip)
         self.parameters = None
         self.Cfunction = MMTK_dynamics.removeRotation
@@ -350,8 +355,8 @@ class RotationRemover(Trajectory.TrajectoryAction):
 #
 def _constraintArrays(universe):
     nc = universe.numberOfDistanceConstraints()
-    constraints = Numeric.zeros((nc, 2), Numeric.Int)
-    const_distances_sq = Numeric.zeros((nc, ), Numeric.Float)
+    constraints = N.zeros((nc, 2), N.Int)
+    const_distances_sq = N.zeros((nc, ), N.Float)
     if nc > 0:
         i = 0
         c_blocks = [0]
@@ -362,9 +367,9 @@ def _constraintArrays(universe):
                 const_distances_sq[i] = c[2]*c[2]
                 i = i + 1
             c_blocks.append(i)
-        c_blocks = Numeric.array(c_blocks)
+        c_blocks = N.array(c_blocks)
     else:
-        c_blocks = Numeric.zeros((1,), Numeric.Int)
+        c_blocks = N.zeros((1,), N.Int)
     return constraints, const_distances_sq, c_blocks
 
 #
