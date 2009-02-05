113f4b24ed11f549ea9761a5d6ae543835a676ff
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2009-02-05 18:20:12 +0100

    Revision of MMTK.Field - Converted docstrings to epytext - Import of other MMTK modules made explicit - Old-style classes converted to new-style - Type tests converted to isinstance calls - Replaced Numeric by Scientific.N

--- MMTK/Field.py
+++ MMTK/Field.py
@@ -1,10 +1,13 @@
 # This module defines scalar and vector fields in molecular systems
 #
 # Written by Konrad Hinsen
-# last revision: 2007-5-22
+# last revision: 2009-2-5
 #
 
-"""This module defines field objects that are useful in the analysis
+"""
+Scalar and vector fields in molecular systems
+
+This module defines field objects that are useful in the analysis
 and visualization of collective motions in molecular systems. Atomic
 quantities characterizing collective motions vary slowly in space, and
 can be considered functions of position instead of values per atom.
@@ -15,26 +18,29 @@ regular grid. In addition to permitting the application of vector
 analysis methods to atomic quantities, the introduction of
 fields is a valuable visualization aid, because information defined on
 a coarse regular grid can be added to a picture of a molecular system
-without overloading it. See also the example
-Example:Miscellaneous:vector_field.py.
+without overloading it.
 """
 
-import Collections, ParticleProperties, Visualization
+__docformat__ = 'epytext'
+
+from MMTK import Collections, ParticleProperties, Visualization
 from Scientific.Visualization import Color
 from Scientific.Geometry import Vector, TensorAnalysis
-from Scientific import N as Numeric
+from Scientific import N
 
-class AtomicField:
 
-    """A field whose values are determined by atomic quantities
+class AtomicField(object):
 
-    This is an Glossary:abstract-base-class. To create field objects,
+    """
+    A field whose values are determined by atomic quantities
+
+    This is an abstract base class. To create field objects,
     use one of its subclasses.
     """
 
     def __init__(self, system, grid_size, values):
         self.system = system
-        if type(grid_size) == type(()):
+        if isinstance(grid_size, tuple):
             self.box, self.field, self.points, self.colors = grid_size
         else:
             if values.data_rank != 1:
@@ -55,28 +61,27 @@ class AtomicField:
             color = default_color
             for a in atoms:
                 d = a.position()-center
-                weight = Numeric.exp(-d*d/self.box.partition_size**2)
+                weight = N.exp(-d*d/self.box.partition_size**2)
                 v = v + weight*data[a]
                 total_weight = total_weight + weight
                 c = default_color
                 try: c = a.color
                 except AttributeError: pass
-                if type(c) == type(''):
+                if isinstance(c, str):
                     c = Color.ColorByName(c)
                 color = color + c
             values.append(v/total_weight)
             colors.append(color)
-        min = Numeric.minimum.reduce(points)
-        max = Numeric.maximum.reduce(points)
-        axes = (Numeric.arange(min[0], max[0]+1, self.box.partition_size),
-                Numeric.arange(min[1], max[1]+1, self.box.partition_size),
-                Numeric.arange(min[2], max[2]+1, self.box.partition_size))
-        array = Numeric.zeros(tuple(map(len, axes)) + data.value_rank*(3,),
-                              Numeric.Float)
-        inside = Numeric.zeros(tuple(map(len, axes)), Numeric.Float)
+        min = N.minimum.reduce(points)
+        max = N.maximum.reduce(points)
+        axes = (N.arange(min[0], max[0]+1, self.box.partition_size),
+                N.arange(min[1], max[1]+1, self.box.partition_size),
+                N.arange(min[2], max[2]+1, self.box.partition_size))
+        array = N.zeros(tuple(map(len, axes)) + data.value_rank*(3,), N.Float)
+        inside = N.zeros(tuple(map(len, axes)), N.Float)
         for p, v in zip(points, values):
-            indices = Numeric.floor((p-min)/self.box.partition_size+0.5)
-            indices = tuple(indices.astype(Numeric.Int))
+            indices = N.floor((p-min)/self.box.partition_size+0.5)
+            indices = tuple(indices.astype(N.Int))
             array[indices] = v
             inside[indices] = 1.
         self.field = self.field_class(axes, array, data.zero())
@@ -97,9 +102,10 @@ class AtomicField:
         return self.field(point)
 
     def particleValues(self):
-        """Returns the values of the field at the positions of the
-        atoms in an appropriate subclass of
-        Class:MMTK.ParticleProperties.ParticleProperty."""
+        """
+        @returns: the values of the field at the positions of the atoms
+        @rtype: L{MMTK.ParticleProperties.ParticleProperty}
+        """
         universe = self.system.universe()
         rank = self.field.rank
         if rank == 0:
@@ -113,19 +119,30 @@ class AtomicField:
         return v
 
     def writeToFile(self, filename, scale = 1., color = None):
-        """Writes a graphical representation of the field
-        to the VRML file named by |filename|, multiplying
-        all values by |scale|. |color| permits the choice
-        of a color for the graphics objects."""
+        """
+        Writes a graphical representation of the field to a VRML file.
+
+        @param filename: the name of the destinatin file
+        @type filename: C{str}
+        @param scale: scale factor applied to all field values
+        @type scale: C{float}
+        @param color: the color for all graphics objects
+        @type color: C{Scientific.Visualization.Color}
+        """
         from Scientific.Visualization import VRML
         objects = self.graphicsObjects(scale=scale, color=color,
                                        graphics_module=VRML)
         VRML.Scene(objects).writeToFile(filename)
 
     def view(self, scale = 1., color = None):
-        """Shows a graphical representation of the field using
-        a VRML viewer. All values are multiplied by |scale|. |color|
-        permits the choice of a color for the graphics objects."""
+        """
+        Shows a graphical representation of the field using a VRML viewer.
+
+        @param scale: scale factor applied to all field values
+        @type scale: C{float}
+        @param color: the color for all graphics objects
+        @type color: C{Scientific.Visualization.Color}
+        """
         from Scientific.Visualization import VRML
         objects = self.graphicsObjects(scale=scale, color=color,
                                        graphics_module=VRML)
@@ -134,55 +151,49 @@ class AtomicField:
 
 class AtomicScalarField(AtomicField, Visualization.Viewable):
 
-    """Scalar field defined by atomic quantities
+    """
+    Scalar field defined by atomic quantities
 
-    A Glossary:Subclass of Class:MMTK.Field.AtomicField and
-    Class:MMTK.Visualization.Viewable.
-
-    Constructor: AtomicScalarField(|system|, |grid_size|, |values|)
-
-    Arguments:
-
-    |system| -- any subset of a molecular system
-
-    |grid_size| -- the spacing of a cubic grid on which the field values
-                   are stored
-
-    |values| -- an object of class Class:MMTK.ParticleScalar containing
-                the atomic values that define the field
-
-    The field values are obtained by averaging the atomic quantities
-    over all atoms in a cube of edge length |grid_size| surrounding
-    each grid point.
-
-    The method graphicsObjects, defined in class
-    Class:MMTK.Visualization.Viewable, returns a small cube for each
-    grid point, whose color indicates the field's value on a symmetric
+    For visualization, scalar fields are represented by a small cube
+    on each grid point whose color indicates the field's value on a symmetric
     red-to-green color scale defined by the range of the field values.
-    Additional keyword options are:
 
-    -- |scale|=factor, to multiply all field values by a factor
-
-    -- |range|=(min, max), to eliminate graphics objects for values
-       that are smaller than min or larger than max
+    Additional keyword options exist for graphics object generation:
+      - scale=factor, to multiply all field values by a factor
+      - range=(min, max), to eliminate graphics objects for values
+        that are smaller than min or larger than max
     """
 
     def __init__(self, system, grid_size, values):
+        """
+        @param system: any subset of a molecular system
+        @param grid_size: the spacing of a cubic grid on which the field
+                          values are defined. The value for a point is obtained
+                          by averaging the atomic quantities over all
+                          atoms in a cube centered on the point.
+        @type grid_size: C{float}
+        @param values: the atomic values that define the field
+        @type values: L{MMTK.ParticleProperties.ParticleScalar}
+        """
         if values is not None and values.value_rank != 0:
             raise TypeError("data not a vector field")
         self.field_class = TensorAnalysis.ScalarField
         AtomicField.__init__(self, system, grid_size, values)
 
     def gradient(self):
-        """Returns an Class:MMTK.Field.AtomicVectorField object representing
-        the gradient of the field."""
+        """
+        @returns: the gradient of the field
+        @rtype: L{MMTK.Field.AtomicVectorField}
+        """
         field = self.field.gradient()
         return AtomicVectorField(self.system, (self.box, field,
                                                self.points, self.colors), None)
 
     def laplacian(self):
-        """Returns an Class:MMTK.Field.AtomicScalarField object representing
-        the laplacian of the field."""
+        """
+        @returns: the laplacian of the field
+        @rtype: L{MMTK.Field.AtomicScalarField}
+        """
         field = self.field.laplacian()
         return AtomicScalarField(self.system, (self.box, field,
                                                self.points, self.colors), None)
@@ -211,74 +222,70 @@ class AtomicScalarField(AtomicField, Visualization.Viewable):
 
 class AtomicVectorField(AtomicField, Visualization.Viewable):
 
-    """Vector field defined by atomic quantities
+    """
+    Vector field defined by atomic quantities
 
-    A Glossary:Subclass of Class:MMTK.Field.AtomicField and
-    Class:MMTK.Visualization.Viewable.
-
-    Constructor: AtomicVectorField(|system|, |grid_size|, |values|)
-
-    Arguments:
-
-    |system| -- any subset of a molecular system
-
-    |grid_size| -- the spacing of a cubic grid on which the field values
-                   are stored
-
-    |values| -- an object of class Class:MMTK.ParticleVector containing
-                the atomic values that define the field
-
-    The field values are obtained by averaging the atomic quantities
-    over all atoms in a cube of edge length |grid_size| surrounding
-    each grid point.
-
-    The method graphicsObjects, defined in class
-    Class:MMTK.Visualization.Viewable, returns a small arrow for each
-    grid point. The arrow starts at the grid point and represents
+    For visualization, scalar fields are represented by a small arrow
+    on each grid point. The arrow starts at the grid point and represents
     the vector value at that point.
-    Additional keyword options are:
-
-    -- |scale|=factor, to multiply all field values by a factor
-
-    -- |diameter|=number, to define the diameter of the arrow
-       objects (default: 1.)
-
-    -- |range|=(min, max), to eliminate graphics objects for values
-       whose lengths are smaller than min or larger than max
-
-    -- |color|=string, to define the color of the arrows
+    
+    Additional keyword options exist for graphics object generation:
+      - scale=factor, to multiply all field values by a factor
+      - diameter=number, to define the diameter of the arrow objects
+                         (default: 1.)
+      - range=(min, max), to eliminate graphics objects for values
+        that are smaller than min or larger than max
+      - color=string, to define the color of the arrows by a color name
     """
 
     def __init__(self, system, grid_size, values):
+        """
+        @param system: any subset of a molecular system
+        @param grid_size: the spacing of a cubic grid on which the field
+                          values are defined. The value for a point is obtained
+                          by averaging the atomic quantities over all
+                          atoms in a cube centered on the point.
+        @type grid_size: C{float}
+        @param values: the atomic values that define the field
+        @type values: L{MMTK.ParticleProperties.ParticleVector}
+        """
         if values is not None and values.value_rank != 1:
             raise TypeError("data not a vector field")
         self.field_class = TensorAnalysis.VectorField
         AtomicField.__init__(self, system, grid_size, values)
 
     def length(self):
-        """Returns an Class:MMTK.Field.AtomicScalarField object representing
-        the length of the field vectors."""
+        """
+        @returns: a field of the length of the field vectors
+        @rtype: L{MMTK.Field.AtomicScalarField}
+        """
         field = self.field.length()
         return AtomicScalarField(self.system, (self.box, field,
                                                self.points, self.colors), None)
 
     def divergence(self):
-        """Returns an Class:MMTK.Field.AtomicScalarField object representing
-        the divergence of the field."""
+        """
+        @returns: the divergence of the field
+        @rtype: L{MMTK.Field.AtomicScalarField}
+        """
         field = self.field.divergence()
         return AtomicScalarField(self.system, (self.box, field,
                                                self.points, self.colors), None)
 
     def curl(self):
-        """Returns an Class:MMTK.Field.AtomicVectorField object representing
-        the curl of the field."""
+        """
+        @returns: the curl of the field
+        @rtype: L{MMTK.Field.AtomicVectorField}
+        """
         field = self.field.curl()
         return AtomicVectorField(self.system, (self.box, field,
                                                self.points, self.colors), None)
 
     def laplacian(self):
-        """Returns an Class:MMTK.Field.AtomicVectorField object representing
-        the laplacian of the field."""
+        """
+        @returns: the laplacian of the field
+        @rtype: L{MMTK.Field.AtomicVectorField}
+        """
         field = self.field.curl()
         return AtomicVectorField(self.system, (self.box, field,
                                                self.points, self.colors), None)
