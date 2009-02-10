e93f10d35dea43546eb152b60efbf1ce28564513
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2009-02-10 10:47:44 +0100

    Revision of MMTK.FourierBasis - Converted docstrings to epytext - Import of other MMTK modules made explicit - Old-style classes converted to new-style - Replaced Numeric by Scientific.N - Some loops and calls to map() replaced by list comprehensions - Boolean use of 0/1 changed to False/True

--- MMTK/FourierBasis.py
+++ MMTK/FourierBasis.py
@@ -1,87 +1,88 @@
 # Fourier basis for low-frequency normal mode calculations.
 #
 # Written by Konrad Hinsen
-# last revision: 2006-11-27
+# last revision: 2009-2-10
 #
 
-"""This module provides a basis that is suitable for the
+"""
+Fourier basis for low-frequency normal mode calculations
+
+This module provides a basis that is suitable for the
 calculation of low-frequency normal modes. The basis is
 derived from vector fields whose components are stationary
 waves in a box surrounding the system. For a description
-see [Article:Hinsen1998].
+see
+     K. Hinsen
+     Analysis of domain motions by approximate normal mode calculations
+     Proteins 33 (1998): 417-429
 """
 
-import ParticleProperties
+from MMTK import ParticleProperties
 from Scientific.Geometry import Vector
-from Scientific import N as Numeric
+from Scientific import N
 
-class FourierBasis:
+class FourierBasis(object):
 
-    """Collective-motion basis for low-frequency normal mode calculations
+    """
+    Collective-motion basis for low-frequency normal mode calculations
 
-    To be used with Class:MMTK.NormalModes.SubspaceNormalModes.
-
-    Constructor: FourierBasis(|universe|, |cutoff|)
-
-    Arguments:
-
-    |universe| -- the universe for which the basis will be used
-
-    |cutoff| -- the wavelength cutoff. A smaller value means a larger basis.
-
-    A FourierBasis object behaves like a sequence of
-    Class:MMTK.ParticleVector objects. The vectors are *not*
-    orthonormal, because orthonormalization is handled automatically
-    by the class Class:MMTK.NormalModes.SubspaceNormalModes.
+    A FourierBasis behaves like a sequence of
+    L{MMTK.ParticleProperties.ParticleVector} objects. The vectors are
+    B{not} orthonormal, because orthonormalization is handled
+    automatically by the normal mode class.
     """
 
     def __init__(self, universe, cutoff):
+        """
+        @param universe: the universe for which the basis will be used
+        @type universe: L{MMTK.Universe.Universe}
+        @param cutoff: the wavelength cutoff. A smaller value yields
+                       a larger basis.
+        @type cutoff: C{float}
+        """
         p1, p2 = universe.boundingBox()
         p2 = p2 + Vector(cutoff, cutoff, cutoff)
         l = (p2-p1).array
-        n_max = (0.5*l/cutoff+0.5).astype(Numeric.Int)
+        n_max = (0.5*l/cutoff+0.5).astype(N.Int)
 
-        wave_numbers = []
-        for nx in range(-n_max[0], n_max[0]+1):
-            for ny in range(-n_max[1], n_max[1]+1):
-                for nz in range(-n_max[2], n_max[2]+1):
-                    if (nx/l[0])**2 + (ny/l[1])**2 + (nz/l[2])**2 \
-                                    < 0.25/cutoff**2:
-                        wave_numbers.append((nx, ny, nz))
+        wave_numbers = [(nx, ny, nz)
+                        for nx in range(-n_max[0], n_max[0]+1)
+                        for ny in range(-n_max[1], n_max[1]+1)
+                        for nz in range(-n_max[2], n_max[2]+1)
+                        if (nx/l[0])**2 + (ny/l[1])**2 + (nz/l[2])**2 \
+                                    < 0.25/cutoff**2]
 
         atoms = universe.atomList()
         natoms = len(atoms)
-        basis = Numeric.zeros((3*len(wave_numbers)+3, natoms, 3),
-                              Numeric.Float)
+        basis = N.zeros((3*len(wave_numbers)+3, natoms, 3), N.Float)
         cm = universe.centerOfMass()
         i = 0
-        for rotation in [Vector(1.,0.,0.), Vector(0.,1.,0.),
-                         Vector(0.,0.,1.)]:
+        for rotation in [Vector(1.,0.,0.), Vector(0.,1.,0.), Vector(0.,0.,1.)]:
             v = ParticleProperties.ParticleVector(universe, basis[i])
             for a in atoms:
                 v[a] = rotation.cross(a.position()-cm)
-            i = i + 1
+            i += i
         conf = universe.configuration().array-p1.array
         for n in wave_numbers:
-            k = 2.*Numeric.pi*Numeric.array(n)/l
+            k = 2.*N.pi*N.array(n)/l
             w = self._w(conf[:, 0], k[0]) * self._w(conf[:, 1], k[1]) * \
                 self._w(conf[:, 2], k[2])
             basis[i, :, 0] = w
             basis[i+1, :, 1] = w
             basis[i+2, :, 2] = w
-            i = i + 3
+            i += 3
 
         self.array = basis
         self.universe = universe
 
-    __safe_for_unpickling__ = 1
-    __had_initargs__ = 1
+    __safe_for_unpickling__ = True
+    __had_initargs__ = True
 
     def _w(self, x, k):
         if k < 0:
-            return Numeric.sin(-k*x)
+            return N.sin(-k*x)
         else:
-            return Numeric.cos(k*x)
+            return N.cos(k*x)
 
     def __len__(self):
         return self.array.shape[0]
@@ -94,27 +95,41 @@ class FourierBasis:
 # Estimate number of basis vectors for a given cutoff
 
 def countBasisVectors(universe, cutoff):
-    """Returns the number of basis vectors in a FourierBasis for
-    the given |universe| and |cutoff|."""
+    """
+    Estimate the number of basis vectors for a given universe and cutoff
+    @param universe: the universe
+    @type universe: L{MMTK.Universe.Universe}
+    @param cutoff: the wavelength cutoff. A smaller value yields a larger basis.
+    @type cutoff: C{float}
+    @returns: the number of basis vectors in a FourierBasis
+    @rtype: C{int}
+    """
     p1, p2 = universe.boundingBox()
     p2 = p2 + Vector(cutoff, cutoff, cutoff)
     l = (p2-p1).array
-    n_max = (0.5*l/cutoff+0.5).astype(Numeric.Int)
-    wave_numbers = []
+    n_max = (0.5*l/cutoff+0.5).astype(N.Int)
+    n_wave_numbers = 0
     for nx in range(-n_max[0], n_max[0]+1):
         for ny in range(-n_max[1], n_max[1]+1):
             for nz in range(-n_max[2], n_max[2]+1):
                 if (nx/l[0])**2 + (ny/l[1])**2 + (nz/l[2])**2 < 0.25/cutoff**2:
-                    wave_numbers.append((nx, ny, nz))
-    return 3*len(wave_numbers)+3
+                    n_wave_numbers += 1
+    return 3*n_wave_numbers+3
 
 
 # Estimate cutoff for a given number of basis vectors
 
 def estimateCutoff(universe, nmodes):
-    """Returns an estimate for the cutoff that will yield a basis of
-    |nmodes| vectors for the given |universe|. The two return values
-    are the cutoff and the precise number of basis vectors for this cutoff."""
+    """
+    Estimate the cutoff that yields a given number of basis vectors
+    for a given universe.
+    @param universe: the universe
+    @type universe: L{MMTK.Universe.Universe}
+    @param nmodes: the number of basis vectors in a FourierBasis
+    @type nmodes: C{int}
+    @returns: the wavelength cutoff and the precise number of basis vectors
+    @rtype: C{tuple} C{(float, int)}
+    """
     natoms = universe.numberOfCartesianCoordinates()
     if nmodes > natoms:
         nmodes = 3*natoms
@@ -126,13 +141,13 @@ def estimateCutoff(universe, nmodes):
         nmodes_opt = nmodes
         nmodes = countBasisVectors(universe, cutoff)
         while nmodes > nmodes_opt:
-            cutoff = cutoff + 0.1
+            cutoff += 0.1
             if cutoff > cutoff_max:
                 cutoff = cutoff_max
                 break
             nmodes = countBasisVectors(universe, cutoff)
         while nmodes < nmodes_opt:
-            cutoff = cutoff - 0.1
+            cutoff -= 0.1
             if cutoff < 0.1:
                 cutoff = 0.1
                 break
