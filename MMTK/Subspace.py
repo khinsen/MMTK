dfb39a1901793855cf2666d8d635b527f933f85d
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2009-02-12 18:48:23 +0100

    Revision of MMTK.Subspace - Converted docstrings to epytext - Import of other MMTK modules made explicit - Old-style classes converted to new-style

--- MMTK/Subspace.py
+++ MMTK/Subspace.py
@@ -1,16 +1,24 @@
 # This module implements subspaces for motion analysis etc.
 #
 # Written by Konrad Hinsen
-# last revision: 2007-5-31
+# last revision: 2009-2-12
 #
 
-"""This module implements subspaces for infinitesimal (or finite
+"""
+Linear subspaces for infinitesimal motions
+
+This module implements subspaces for infinitesimal (or finite
 small-amplitude) atomic motions. They can be used in normal mode
-calculations (see example Example:NormalModes:constrained_modes.py) or
-for analyzing complex motions [Article:Hinsen1999a].
+calculations or for analyzing complex motions. For an explanation, see:
+
+ -  K. Hinsen and G.R. Kneller
+    Projection methods for the analysis of complex motions in macromolecules
+    Mol. Sim. 23:275-292 (2000)
 """
 
-import Utility, ParticleProperties
+__docformat__ = 'epytext'
+
+from MMTK import Utility, ParticleProperties
 from Scientific.Geometry import Vector
 from Scientific import N
 
@@ -61,7 +69,7 @@ del array_package
 #
 # A set of particle vectors that define a subspace
 #
-class ParticleVectorSet:
+class ParticleVectorSet(object):
 
     def __init__(self, universe, data):
         self.universe = universe
@@ -82,22 +90,21 @@ class ParticleVectorSet:
         return ParticleProperties.ParticleVector(self.universe, self.array[i])
 
 
-class Subspace:
+class Subspace(object):
 
-    """Subspace of infinitesimal atomic motions
-
-    Constructor: Subspace(|universe|, |vectors|)
-
-    Arguments:
-
-    |universe| -- the universe for which the subspace is created
-
-    |vectors| -- a list of Class:MMTK.ParticleVector objects
-                 that define the subspace. They need not be orthogonal
-                 or linearly independent.
+    """
+    Subspace of infinitesimal atomic motions
     """
 
     def __init__(self, universe, vectors):
+        """
+        @param universe: the universe for which the subspace is created
+        @type universe: L{MMTK.Universe.Universe}
+        @param vectors: a list of L{MMTK.ParticleProperties.ParticleVector}
+                        objects that define the subspace. They need not be
+                        orthogonal or linearly independent.
+        @type vectors: C{list}
+        """
         self.universe = universe
         self.vectors = vectors
         self._basis = None
@@ -112,11 +119,14 @@ class Subspace:
         return self.vectors[item]
 
     def getBasis(self):
-        """Returns a basis for the subspace, which is obtained
-        by orthonormalization of the input vectors using Singular
-        Value Decomposition. The basis consists of a sequence
-        of Class:MMTK.ParticleVector objects that are orthonormal
-        in configuration space."""
+        """
+        Construct a basis for the subspace by orthonormalization of
+        the input vectors using Singular Value Decomposition. The
+        basis consists of a sequence of
+        L{MMTK.ParticleProperties.ParticleVector}
+        objects that are orthonormal in configuration space.
+        @returns: the basis
+        """
         if self._basis is None:
             basis = N.array([v.array for v in self.vectors], N.Float)
             shape = basis.shape
@@ -156,8 +166,11 @@ class Subspace:
         return self._basis
 
     def projectionOf(self, vector):
-        """Returns the projection of |vector| (a Class:MMTK.ParticleVector
-        object) onto the subspace."""
+        """
+        @param vector: a particle vector
+        @type vector: L{MMTK.ParticleProperties.ParticleVector}
+        @returns: the projection of the vector onto the subspace.
+        """
         vector = vector.array
         basis = self.getBasis().array
         p = N.zeros(vector.shape, N.Float)
@@ -166,31 +179,31 @@ class Subspace:
         return ParticleProperties.ParticleVector(self.universe, p)
 
     def projectionComplementOf(self, vector):
-        """Returns the projection of |vector| (a Class:MMTK.ParticleVector
-        object) onto the orthogonal complement of the subspace."""
+        """
+        @param vector: a particle vector
+        @type vector: L{MMTK.ParticleProperties.ParticleVector}
+        @returns: the projection of the vector onto the orthogonal complement
+                  of the subspace.
+        """
         return vector - self.projectionOf(vector)
 
 
 class RigidMotionSubspace(Subspace):
 
-    """Subspace of rigid-body motions
-
-    A Glossary:Subclass of Class:MMTK.Subspace.Subspace.
+    """
+    Subspace of rigid-body motions
 
     A rigid-body motion subspace is the subspace which contains
     the rigid-body motions of any number of chemical objects.
-
-    Constructor: RigidMotionSubspace(|universe|, |objects|)
-
-    Arguments:
-
-    |universe| -- the universe for which the subspace is created
-
-    |objects| -- a sequence of objects whose rigid-body motion is
-                 included in the subspace
     """
 
     def __init__(self, universe, objects):
+        """
+        @param universe: the universe for which the subspace is created
+        @type universe: L{MMTK.Universe.Universe}
+        @param objects: a sequence of objects whose rigid-body motion is
+                        included in the subspace
+        """
         if not Utility.isSequenceObject(objects):
             objects = [objects]
         vectors = []
@@ -226,26 +239,22 @@ class RigidMotionSubspace(Subspace):
 
 class PairDistanceSubspace(Subspace):
 
-    """Subspace of pair-distance motions
-
-    A Glossary:Subclass of Class:MMTK.Subspace.Subspace.
+    """
+    Subspace of pair-distance motions
 
     A pair-distance motion subspace is the subspace which contains
     the relative motions of any number of atom pairs along
     their distance vector, e.g. bond elongation between two
     bonded atoms.
-
-    Constructor: PairDistanceSubspace(|universe|, |atom_pairs|)
-
-    Arguments:
-
-    |universe| -- the universe for which the subspace is created
-
-    |atom_pairs| -- a sequence of atom pairs whose distance-vector
-                    motion is included in the subspace
     """
 
     def __init__(self, universe, atom_pairs):
+        """
+        @param universe: the universe for which the subspace is created
+        @type universe: L{MMTK.Universe.Universe}
+        @param atom_pairs: a sequence of atom pairs whose distance-vector
+                           motion is included in the subspace
+        """
         vectors = []
         for atom1, atom2 in atom_pairs:
             v = ParticleProperties.ParticleVector(universe)
