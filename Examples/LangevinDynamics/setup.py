7383dfa4a416ef7fb37587a9b941a6721953b2b5
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2010-06-02 19:10:36 +0200

    Fix setup.py for LangevinDynamics to work with NumPy

--- Examples/LangevinDynamics/setup.py
+++ Examples/LangevinDynamics/setup.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python
 
-package_name = "MMTK-LangevinDynamics"
-version_number = "1.0"
-
 from distutils.core import setup, Extension
-import sys
+import os, sys
+
+compile_args = []
+include_dirs = ['.']
+
+from Scientific import N
+try:
+    num_package = N.package
+except AttributeError:
+    num_package = "Numeric"
+if num_package == "NumPy":
+    compile_args.append("-DNUMPY=1")
+    if sys.platform == 'win32':
+        include_dirs.append(os.path.join(sys.prefix,
+                            "Lib/site-packages/numpy/core/include"))
+    else:
+        include_dirs.append(os.path.join(sys.prefix,
+                            "lib/python%s.%s/site-packages/numpy/core/include"
+                             % sys.version_info [:2]))
 
 setup (name = "MMTK-LangevinDynamics",
-       version = "1.2",
+       version = "2.7",
        description = "Langevin dynamics module for the Molecular Modelling Toolkit",
        author = "Konrad Hinsen",
-       author_email = "hinsen@llb.saclay.cea.fr",
+       author_email = "hinsen@cnrs-orleans.fr",
        url = "http://dirac.cnrs-orleans.fr/MMTK/",
-       license = "LGPL",
+       license = "CeCILL-C",
 
        py_modules = ['LangevinDynamics'],
        ext_modules = [Extension('MMTK_langevin',
                                 ['./MMTK_langevin.c', 'ranf.c', 'pmath_rng.c'],
-                                include_dirs=['./']),
+                                extra_compile_args = compile_args,
+                                include_dirs=include_dirs),
                       ],
        )
