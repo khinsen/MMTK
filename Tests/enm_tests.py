9c43c2605fb5d3ac3972767de4f02302c8acc911
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2010-09-07 20:07:24 +0200

    New test case for energy evaluation with subsets
    --HG--
    branch : path_integrals

--- Tests/enm_tests.py
+++ Tests/enm_tests.py
@@ -9,38 +9,7 @@ from MMTK import *
 from MMTK.ForceFields import CalphaForceField, DeformationForceField, \
                              AnisotropicNetworkForceField
 from MMTK.Proteins import Protein
-
-class SubsetTest(object):
-
-    def test_singleSubset(self):
-        outside = set(self.universe.atomList())-set(self.subset1.atomList())
-        e, fc = self.universe.energyAndForceConstants(self.subset1)
-        for a1 in outside:
-            for a2 in outside:
-                self.assert_((fc[a1,a2].array == 0).all())
-        for a1 in self.subset1.atomList():
-            for a2 in self.subset1.atomList():
-                self.assert_((fc[a1,a2].array != 0).any())
-
-    def test_twoSubsets(self):
-        inside = set(self.subset1.atomList()) | set(self.subset2.atomList())
-        outside = set(self.universe.atomList())-inside
-        e, fc = self.universe.energyAndForceConstants(self.subset1, self.subset2)
-        for a1 in self.subset1.atomList():
-            for a2 in self.subset1.atomList():
-                if a1 != a2:
-                    self.assert_((fc[a1,a2].array == 0).all())
-        for a1 in self.subset2.atomList():
-            for a2 in self.subset2.atomList():
-                if a1 != a2:
-                    self.assert_((fc[a1,a2].array == 0).all())
-        for a1 in self.subset1.atomList():
-            for a2 in self.subset2.atomList():
-                self.assert_((fc[a1,a2].array != 0).any())
-        for a1 in outside:
-            for a2 in outside:
-                self.assert_((fc[a1,a2].array == 0).all())
-
+from subsets import SubsetTest
 
 class CalphaFFSubsetTest(unittest.TestCase,
                          SubsetTest):
---
+++ Tests/subsets.py
@@ -0,0 +1,39 @@
+# Subset energy tests
+#
+# Written by Konrad Hinsen
+# last revision: 2010-9-7
+#
+
+from Scientific import N
+
+class SubsetTest(object):
+
+    def test_singleSubset(self):
+        outside = set(self.universe.atomList())-set(self.subset1.atomList())
+        e, fc = self.universe.energyAndForceConstants(self.subset1)
+        for a1 in outside:
+            for a2 in outside:
+                self.assert_((N.fabs(fc[a1,a2].array) < 1.e-14).all())
+        for a1 in self.subset1.atomList():
+            for a2 in self.subset1.atomList():
+                self.assert_((N.fabs(fc[a1,a2].array) > 1.e-14).any())
+
+    def test_twoSubsets(self):
+        inside = set(self.subset1.atomList()) | set(self.subset2.atomList())
+        outside = set(self.universe.atomList())-inside
+        e, fc = self.universe.energyAndForceConstants(self.subset1, self.subset2)
+        for a1 in self.subset1.atomList():
+            for a2 in self.subset1.atomList():
+                if a1 != a2:
+                    self.assert_((N.fabs(fc[a1,a2].array) < 1.e-14).all())
+        for a1 in self.subset2.atomList():
+            for a2 in self.subset2.atomList():
+                if a1 != a2:
+                    self.assert_((N.fabs(fc[a1,a2].array) < 1.e-14).all())
+        for a1 in self.subset1.atomList():
+            for a2 in self.subset2.atomList():
+                self.assert_((N.fabs(fc[a1,a2].array) > 1.e-14).any())
+        for a1 in outside:
+            for a2 in outside:
+                self.assert_((N.fabs(fc[a1,a2].array) < 1.e-14).all())
+
