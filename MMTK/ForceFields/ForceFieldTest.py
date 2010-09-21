6afd78c48681f9af4f4fcbb98df15bf77f626b59
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2010-09-21 10:41:39 +0200

    Extend force field test code to path integral systems
    --HG--
    branch : path_integrals

--- MMTK/ForceFields/ForceFieldTest.py
+++ MMTK/ForceFields/ForceFieldTest.py
@@ -33,18 +33,19 @@ def gradientTest(universe, atoms = None, delta = 0.0001):
     if atoms is None:
         atoms = universe.atomList()
     for a in atoms:
-        print a
-        print grad[a]
-        num_grad = []
-        for v in [ex, ey, ez]:
-            x = a.position()
-            a.setPosition(x+delta*v)
-            eplus = universe.energy()
-            a.setPosition(x-delta*v)
-            eminus = universe.energy()
-            a.setPosition(x)
-            num_grad.append(0.5*(eplus-eminus)/delta)
-        print Vector(num_grad)
+        for b in a.beads():
+            print a, " bead ", b.bead_number
+            print grad[b]
+            num_grad = []
+            for v in [ex, ey, ez]:
+                x = b.position()
+                b.setPosition(x+delta*v)
+                eplus = universe.energy()
+                b.setPosition(x-delta*v)
+                eminus = universe.energy()
+                b.setPosition(x)
+                num_grad.append(0.5*(eplus-eminus)/delta)
+            print Vector(num_grad)
 
 #
 # Check consistency of gradients and force constants.
@@ -66,18 +67,19 @@ def forceConstantTest(universe, atoms = None, delta = 0.0001):
         atoms = universe.atomList()
     for a1, a2 in itertools.chain(itertools.izip(atoms, atoms),
                                   Utility.pairs(atoms)):
-        print a1, a2
-        print fc[a1, a2]
-        num_fc = []
-        for v in [ex, ey, ez]:
-            x = a1.position()
-            a1.setPosition(x+delta*v)
-            e_plus, grad_plus = universe.energyAndGradients()
-            a1.setPosition(x-delta*v)
-            e_minus, grad_minus = universe.energyAndGradients()
-            a1.setPosition(x)
-            num_fc.append(0.5*(grad_plus[a2]-grad_minus[a2])/delta)
-        print N.array(map(lambda a: a.array, num_fc))
+        for b1, b2 in zip(a1.beads(), a2.beads()):
+            print a1, a2, " bead ", b1.bead_number
+            print fc[b1, b2]
+            num_fc = []
+            for v in [ex, ey, ez]:
+                x = b1.position()
+                b1.setPosition(x+delta*v)
+                e_plus, grad_plus = universe.energyAndGradients()
+                b1.setPosition(x-delta*v)
+                e_minus, grad_minus = universe.energyAndGradients()
+                b1.setPosition(x)
+                num_fc.append(0.5*(grad_plus[b2]-grad_minus[b2])/delta)
+            print N.array(map(lambda a: a.array, num_fc))
 
 
 
