b49b13c1416d7ccbc7c7d5d5d3418cc88bcf8cf6
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2010-12-23 17:59:20 +0100

    Test case for superposition

--- Tests/basic_tests.py
+++ Tests/basic_tests.py
@@ -5,6 +5,8 @@
 
 import unittest
 import MMTK
+from MMTK.Proteins import Protein
+from Scientific import N
 
 class GroupOfAtomTest(unittest.TestCase):
 
@@ -22,8 +24,31 @@ class GroupOfAtomTest(unittest.TestCase):
                          self.results['numberOfAtoms'])
 
 
+class SuperpositionTest(unittest.TestCase):
+
+    """
+    Test the findTransformation method
+    """
+
+    def test_rotation_translation(self):
+        for m in [MMTK.Molecule('water'), Protein('bala1')]:
+            universe = MMTK.InfiniteUniverse()
+            universe.addObject(m)
+            ref_conf = universe.copyConfiguration()
+            universe.translateBy(MMTK.Vector(0.1, -1.3, 1.2))
+            universe.rotateAroundOrigin(MMTK.Vector(0., 1., 1.), 0.7)
+            tr, rms = universe.findTransformation(ref_conf)
+            self.assert_(abs(rms) < 1.e-5)
+            universe.applyTransformation(tr)
+            self.assert_(universe.rmsDifference(ref_conf) < 1.e-5)
+            axis, angle = tr.rotation().axisAndAngle()
+            self.assert_(abs(angle - 0.7) < 1.e-5)
+            self.assert_(abs(abs(N.cos(axis.angle(MMTK.Vector(0., 1., 1.))))-1)
+                         < 1.e-5)
+
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(GroupOfAtomTest)
+    return unittest.TestLoader().loadTestsFromTestCase(SuperpositionTest)
 
 if __name__ == '__main__':
     unittest.main()
