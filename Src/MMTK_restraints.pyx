b8ec46c9ec880bde4235673d484e33a75d6448ee
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2012-07-04 17:15:46 +0200

    Generalized HarmonicDistanceRestraint to centers of mass, applied some fixes to HarmonicTrapForceField

--- Src/MMTK_restraints.pyx
+++ Src/MMTK_restraints.pyx
@@ -8,13 +8,16 @@ include "MMTK/core.pxi"
 include "MMTK/universe.pxi"
 include 'MMTK/forcefield.pxi'
 
+cdef extern from "math.h":
+    double sqrt(double)
+
 import numpy as N
 cimport numpy as N
 
 #
 # Trap potential (harmonic restraint to a fixed point in space)
 #
-cdef class HarmonicTrapTerm(EnergyTerm):
+cdef class HarmonicCMTrapTerm(EnergyTerm):
 
     cdef N.ndarray atom_indices, weights
     cdef double ref_x, ref_y, ref_z
@@ -26,8 +29,8 @@ cdef class HarmonicTrapTerm(EnergyTerm):
                  reference, force_constant):
         cdef N.ndarray[N.float64_t] ref_array
         EnergyTerm.__init__(self, universe,
-                            "harmonic_trap", ("harmonic_trap",))
-        self.eval_func = <void *>HarmonicTrapTerm.evaluate
+                            "harmonic_cm_trap", ("harmonic_cm_trap",))
+        self.eval_func = <void *>HarmonicCMTrapTerm.evaluate
         self.atom_indices = atom_indices
         self.weights = weights
         ref_array = reference.array
@@ -83,3 +86,115 @@ cdef class HarmonicTrapTerm(EnergyTerm):
                     fc [i, 0, j, 0] += kw*weights[i]*weights[j]
                     fc [i, 1, j, 1] += kw*weights[i]*weights[j]
                     fc [i, 2, j, 2] += kw*weights[i]*weights[j]
+
+
+#
+# Harmonic potential between two centers of mass
+#
+cdef class HarmonicCMDistanceTerm(EnergyTerm):
+
+    cdef N.ndarray atom_indices_1, atom_indices_2, weights
+    cdef double k, l0
+
+    def __init__(self, universe,
+                 N.ndarray[N.int_t] atom_indices_1,
+                 N.ndarray[N.int_t] atom_indices_2,
+                 N.ndarray[N.float64_t] weights,
+                 distance,
+                 force_constant):
+        EnergyTerm.__init__(self, universe,
+                            "harmonic_cm_distance", ("harmonic_cm_distance",))
+        self.eval_func = <void *>HarmonicCMDistanceTerm.evaluate
+        self.atom_indices_1 = atom_indices_1
+        self.atom_indices_2 = atom_indices_2
+        self.weights = weights
+        self.l0 = distance
+        self.k = force_constant
+
+    cdef void evaluate(self, PyFFEvaluatorObject *eval,
+                       energy_spec *input, energy_data *energy):
+        cdef vector3 *coordinates, *gradients
+        cdef N.ndarray[N.int_t] atom_indices_1
+        cdef N.ndarray[N.int_t] atom_indices_2
+        cdef N.ndarray[N.float_t] weights
+        cdef N.ndarray[N.float_t, ndim=4] fc
+        cdef tensor3 fcij
+        cdef vector3 cm1, cm2, d
+        cdef double m1, m2, lsq, l, dl, deriv, w
+        cdef int i, j, n, offset
+
+        coordinates = <vector3 *>input.coordinates.data
+        atom_indices_1 = self.atom_indices_1
+        atom_indices_2 = self.atom_indices_2
+        weights = self.weights
+
+        # This code will never be run for a periodic universe, so
+        # it's safe to ignore periodic boundary conditions.
+        m1 = 0.
+        cm1[0] = cm1[1] = cm1[2] = 0.
+        for i in atom_indices_1:
+            m1 += weights[i]
+            for j in range(3):
+                cm1[j] += weights[i]*coordinates[i][j]
+        for j in range(3):
+            cm1[j] /= m1
+
+        m2 = 0.
+        cm2[0] = cm2[1] = cm2[2] = 0.
+        for i in atom_indices_2:
+            m2 += weights[i]
+            for j in range(3):
+                cm2[j] += weights[i]*coordinates[i][j]
+        for j in range(3):
+            cm2[j] /= m2
+
+        for j in range(3):
+            d[j] = cm2[j]-cm1[j]
+        lsq = d[0]*d[0]+d[1]*d[1]+d[2]*d[2]
+        l = sqrt(lsq)
+        dl = l - self.l0
+        energy.energy_terms[self.index] = self.k*dl*dl
+        energy.energy_terms[self.virial_index] -= 2.*self.k*dl*dl
+
+        if energy.gradients != NULL or energy.force_constants != NULL:
+            deriv = 0. if l == 0. else 2.*self.k*dl/l
+
+        if energy.gradients != NULL:
+            gradients = <vector3 *>(<PyArrayObject *> energy.gradients).data
+            deriv = 0. if l == 0. else 2.*self.k*dl/l
+            f1 = self.k*deriv/m1
+            f2 = self.k*deriv/m2
+            for i in atom_indices_1:
+                for j in range(3):
+                    gradients[i][j] -= f1*d[j]*weights[i]
+            for i in atom_indices_2:
+                for j in range(3):
+                    gradients[i][j] += f2*d[j]*weights[i]
+
+        if energy.force_constants != NULL:
+            fc = <object>energy.force_constants
+            for m in range(3):
+                for n in range(3):
+                    fcij[m][n] = (2.*self.k-deriv)*d[m]*d[n]/lsq
+                fcij[m][m] += deriv
+            for i in atom_indices_1:
+                for j in atom_indices_1:
+                    w = weights[i]*weights[j]/(m1*m1)
+                    for m in range(3):
+                        for n in range(3):
+                            fc [i, m, j, n] += w*fcij[m][n]
+            for i in atom_indices_2:
+                for j in atom_indices_2:
+                    w = weights[i]*weights[j]/(m2*m2)
+                    for m in range(3):
+                        for n in range(3):
+                            fc [i, m, j, n] += w*fcij[m][n]
+            for i in atom_indices_1:
+                for j in atom_indices_2:
+                    w = weights[i]*weights[j]/(m1*m2)
+                    for m in range(3):
+                        for n in range(3):
+                            if i < j:
+                                fc [i, m, j, n] -= w*fcij[m][n]
+                            else:
+                                fc [j, m, i, n] -= w*fcij[m][n]
--- Tests/restraint_tests.py
+++ Tests/restraint_tests.py
@@ -101,10 +101,113 @@ class TrapTest(unittest.TestCase):
         subset = Collection([H1, H2])
         self.assertRaises(ValueError, self.universe3.energy, subset)
         
+
+class DistanceTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.universe1 = InfiniteUniverse()
+        atom1 = Atom('C', position=Vector(0.5, 0., 0.))
+        atom2 = Atom('C', position=Vector(-0.5, 0., 0.))
+        self.universe1.addObject(atom1)
+        self.universe1.addObject(atom2)
+        ff = Restraints.HarmonicDistanceRestraint(atom1, atom2,
+                                                   0.9, 1.)
+        self.universe1.setForceField(ff)
+
+        self.universe2 = InfiniteUniverse()
+        atom1 = Atom('C', position=Vector(0.25, 0., 0.))
+        atom2 = Atom('C', position=Vector(0.75, 0., 0.))
+        cluster1 = Collection([atom1, atom2])
+        atom3 = Atom('C', position=Vector(-0.75, 0., 0.))
+        atom4 = Atom('C', position=Vector(-0.25, 0., 0.))
+        cluster2 = Collection([atom3, atom4])
+        self.universe2.addObject(cluster1)
+        self.universe2.addObject(cluster2)
+        ff = Restraints.HarmonicDistanceRestraint(cluster1, cluster2,
+                                                  0.9, 1.)
+        self.universe2.setForceField(ff)
+
+        self.universe3 = InfiniteUniverse()
+        water1 = Molecule('water', position=Vector(0.5, 0., 0.))
+        water2 = Molecule('water', position=Vector(-0.5, 0., 0.))
+        self.universe3.addObject(water1)
+        self.universe3.addObject(water2)
+        ff = Restraints.HarmonicDistanceRestraint(water1, water2,
+                                                  0.9, 1.)
+        self.universe3.setForceField(ff)
+
+    def test_energy_single_atoms(self):
+        e, g, fc = self.universe1.energyGradientsAndForceConstants()
+        self.assertAlmostEqual(e, 0.01, 7)
+        self.assert_((g[0]-Vector(0.2, 0., 0.)).length() < 1.e-5)
+        fc = fc[0, 0]
+        fc_ref = [2., 0.2, 0.2]
+        for i in range(3):
+            self.assertAlmostEqual(fc[i, i], fc_ref[i], 7)
+            for j in range(3):
+                if i != j:
+                    self.assertEqual(fc[i,j], 0.)
+
+    def test_energy_atom_pairs(self):
+        e, g, fc = self.universe2.energyGradientsAndForceConstants()
+        self.assertAlmostEqual(e, 0.01, 7)
+        self.assert_((g[0]-Vector(0.1, 0., 0.)).length() < 1.e-5)
+        self.assert_((g[1]-Vector(0.1, 0., 0.)).length() < 1.e-5)
+        self.assert_((g[2]-Vector(-0.1, 0., 0.)).length() < 1.e-5)
+        self.assert_((g[3]-Vector(-0.1, 0., 0.)).length() < 1.e-5)
+        p1 = [0, 1]
+        p2 = [2, 3]
+        fc_ref = [0.5, 0.05, 0.05]
+        for a1 in range(4):
+            for a2 in range(4):
+                fcp = fc[a1, a2]
+                for i in range(3):
+                    if (a1 in p1 and a2 in p1) or (a1 in p2 and a2 in p2):
+                        self.assertAlmostEqual(fcp[i, i], fc_ref[i], 7)
+                    else:
+                        self.assertAlmostEqual(fcp[i, i], -fc_ref[i], 7)
+                    for j in range(3):
+                        if i != j:
+                            self.assertEqual(fcp[i,j], 0.)
+
+    def test_energy_water(self):
+        e, g, fc = self.universe3.energyGradientsAndForceConstants()
+        fc_ref = [2., 0.2, 0.2]
+        self.assertAlmostEqual(e, 0.01, 7)
+        for m, s in [(self.universe3[0], 1), (self.universe3[1], -1),]:
+            self.assert_((g[m.H1]-g[m.H2]).length() < 1.e-5)
+            self.assert_((g[m.H1]-g[m.O]*m.H1.mass()/m.O.mass()).length()
+                         < 1.e-5)
+            mass = 2*m.H1.mass() + m.O.mass()
+            for a1 in [m.H1, m.H2, m.O]:
+                for a2 in [m.H1, m.H2, m.O]:
+                    fc12 = fc[a1, a2]
+                    f = a1.mass()*a2.mass()/(mass*mass)
+                    for i in range(3):
+                        for j in range(3):
+                            if i == j:
+                                self.assertAlmostEqual(fc12[i, j], f*fc_ref[i], 7)
+                            else:
+                                self.assertEqual(fc12[i,j], 0.)
+
+    def test_subsets(self):
+        m1 = self.universe3[0]
+        m2 = self.universe3[1]
+        subset = Collection([m1.H1, m1.H2, m1.O, m2.H1, m2.H2, m2.O])
+        e = self.universe3.energy(subset)
+        self.assertAlmostEqual(e, 0.01, 7)
+        subset = Collection()
+        e = self.universe3.energy(subset)
+        self.assertAlmostEqual(e, 0., 7)
+        subset = Collection([m1.H1, m2.H2])
+        self.assertRaises(ValueError, self.universe3.energy, subset)
+        
 def suite():
     loader = unittest.TestLoader()
     s = unittest.TestSuite()
     s.addTest(loader.loadTestsFromTestCase(TrapTest))
+    s.addTest(loader.loadTestsFromTestCase(DistanceTest))
     return s
 
 if __name__ == '__main__':
