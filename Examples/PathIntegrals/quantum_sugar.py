1b1929c3d8ce5af33b1ee2a48932990f295ef160
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2014-09-22 10:57:24 -0400

    Fix up quantum sugar example
    --HG--
    branch : path_integrals

--- Examples/PathIntegrals/quantum_sugar.py
+++ Examples/PathIntegrals/quantum_sugar.py
@@ -3,11 +3,9 @@
 
 from MMTK import *
 from MMTK.ForceFields import Amber99ForceField
-from MMTK.PICartesianIntegrator import PICartesianIntegrator, \
-                                       PILangevinCartesianIntegrator
+from MMTK.PICartesianIntegrator import PILangevinCartesianIntegrator
 from MMTK.Trajectory import Trajectory, TrajectoryOutput
 from MMTK.NormalModes import VibrationalModes
-from MMTK import Features
 
 from Scientific import N
 from Scientific.Statistics import mean, standardDeviation
@@ -23,20 +21,8 @@ def timeStep(universe):
     # We want to choose the time step based on the frequency of the
     # fastest vibrations of the classical analog system, i.e. the
     # beads with harmonic interactions. This requires an explicit
-    # representation of the spring terms in the potential. By default,
-    # MMTK does not add the spring terms, because they are calculated
-    # implicitly by the normal-mode integrator. Therefore we replace
-    # the PathIntegrals environment object temporarily by a version
-    # that makes the spring terms explicit.
-    pi = universe.environmentObjectList(Environment.PathIntegrals)[0]
-    pi_temp = Environment.PathIntegrals(pi.temperature, True)
-    universe.removeObject(pi)
-    universe.addObject(pi_temp)
-
+    # representation of the spring terms in the potential.
     modes = VibrationalModes(universe)
-
-    universe.removeObject(pi_temp)
-    universe.addObject(pi)
 
     # Make timestep 1/20 of the period of the fastest vibration
     return 0.05/modes[-1].frequency
@@ -54,9 +40,9 @@ universe.sugar = sugar
 
 # The PathIntegrals environment object defines the temperature
 # used in the spring terms between the beads of each atom.
-# The thermostat in the PILangevinNormalModeIntegrator uses
+# The thermostat in the PILangevinCartesianIntegrator uses
 # the same temperature.
-universe.addObject(Environment.PathIntegrals(temperature))
+universe.addObject(Environment.PathIntegrals(temperature, True))
 
 # Use path integrals with 32 beads for all atoms.
 for atom in universe.atomIterator():
