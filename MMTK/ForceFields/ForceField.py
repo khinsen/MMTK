b50163367c20a4fcb050843784f0a35b63490bd4
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2015-03-20 18:11:22 -0400

    Combine EnergyEvaluator and EnergyEvaluatorParameters
    --HG--
    branch : path_integrals

--- MMTK/ForceFields/ForceField.py
+++ MMTK/ForceFields/ForceField.py
@@ -275,7 +275,7 @@ def addToForceConstants(total_fc, indices, small_fc):
 #
 # High-level energy evaluator (i.e. the Python interface)
 #
-class EnergyEvaluatorParameters(object):
+class _EnergyEvaluator(object):
 
     def __init__(self, universe, force_field, subset1=None, subset2=None):
         if not Universe.isUniverse(universe):
@@ -286,18 +286,18 @@ class EnergyEvaluatorParameters(object):
         self.configuration = self.universe.configuration()
         self.global_data = ForceFieldData()
         self.global_data.set('universe', universe)
-        if subset1 is not None and subset2 is None:
-            subset2 = subset1
+        self.subset1 = subset1
+        self.subset2 = subset2 if subset2 is not None else subset1
 
-        spring_parameters = []
+        self.spring_parameters = []
 
-        if subset1 is None:
+        if self.subset1 is None:
             pi_atoms = [a for a in self.universe.atomIterator() if a.numberOfBeads() > 1]
         else:
-            pi_atoms = list(set(a for a in subset1.atomIterator() if a.numberOfBeads() > 1)
-                            | set(a for a in subset2.atomIterator() if a.numberOfBeads() > 1))
-        if pi_atoms:
+            pi_atoms = list(set(a for a in self.subset1.atomIterator() if a.numberOfBeads() > 1)
+                            | set(a for a in self.subset2.atomIterator() if a.numberOfBeads() > 1))
 
+        if pi_atoms:
             if not self.ff.supportsPathIntegrals():
                 raise ValueError("Some force field term doesn't handle path integrals")
             nbeads = self.universe.maxNumberOfBeads()
@@ -312,12 +312,10 @@ class EnergyEvaluatorParameters(object):
                     nb = a.numberOfBeads()
                     k = float(nbeads*nb)*a.mass() / (beta*beta*Units.hbar*Units.hbar*2.)
                     for b in range(nb):
-                        spring_parameters.append((a.index+b, a.index+(b+1)%nb, 0., k))
-
+                        self.spring_parameters.append((a.index+b, a.index+(b+1)%nb, 0., k))
         else:
-
             nbeads = 1
-            
+
         self.global_data.set('nbeads', nbeads)
         bead_data = N.zeros((self.universe.numberOfPoints(), 2), N.Int16)
         for b in universe.beadIterator():
@@ -325,80 +323,44 @@ class EnergyEvaluatorParameters(object):
             bead_data[b.index, 1] = b.atom.numberOfBeads()
         self.global_data.set('bead_data', bead_data)
 
+
+class EnergyEvaluatorParameters(_EnergyEvaluator):
+
+    def __init__(self, universe, force_field, subset1=None, subset2=None):
+        _EnergyEvaluator.__init__(self, universe, force_field, subset1, subset2)
+
         self.params = self.ff.evaluatorParameters(self.universe,
-                                                  subset1, subset2,
+                                                  self.subset1, self.subset2,
                                                   self.global_data)
         key = 'harmonic_distance_term'
-        self.params[key] = _combine(spring_parameters, self.params.get(key, None))
+        self.params[key] = _combine(self.spring_parameters,
+                                    self.params.get(key, None))
 
 
-class EnergyEvaluator(object):
+class EnergyEvaluator(_EnergyEvaluator):
 
     def __init__(self, universe, force_field, subset1=None, subset2=None,
                  threads=None, mpi_communicator=None):
-        if not Universe.isUniverse(universe):
-            raise TypeError("energy evaluator defined only for universes")
-        self.universe = universe
-        self.universe_version = self.universe._version
-        self.ff = force_field
-        self.configuration = self.universe.configuration()
-        self.global_data = ForceFieldData()
-        self.global_data.set('universe', universe)
-        if subset1 is not None and subset2 is None:
-            subset2 = subset1
+        _EnergyEvaluator.__init__(self, universe, force_field, subset1, subset2)
 
         terms = []
-
-        if subset1 is None:
-            pi_atoms = [a for a in self.universe.atomIterator() if a.numberOfBeads() > 1]
-        else:
-            pi_atoms = list(set(a for a in subset1.atomIterator() if a.numberOfBeads() > 1)
-                            | set(a for a in subset2.atomIterator() if a.numberOfBeads() > 1))
-        if pi_atoms:
-
-            if not self.ff.supportsPathIntegrals():
-                raise ValueError("Some force field term doesn't handle path integrals")
-            nbeads = self.universe.maxNumberOfBeads()
-            pi_environments = self.universe.environmentObjectList(
-                                                Environment.PathIntegrals)
-            if len(pi_environments) == 1:
-                beta = pi_environments[0].beta
-            else:
-                raise ValueError('exactly one path integral environment required')
-            if pi_environments[0].include_spring_terms:
-                indices = []
-                parameters = []
-                for a in pi_atoms:
-                    nb = a.numberOfBeads()
-                    k = float(nbeads*nb)*a.mass() / (beta*beta*Units.hbar*Units.hbar*2.)
-                    for b in range(nb):
-                        indices.append([a.index+b, a.index+(b+1)%nb])
-                        parameters.append([0., k])
-                terms.append(HarmonicDistanceTerm(universe._spec,
-                                                  N.array(indices),
-                                                  N.array(parameters),
-                                                  "path integral spring"))
-
-        else:
-
-            nbeads = 1
-
-        self.global_data.set('nbeads', nbeads)
-        bead_data = N.zeros((self.universe.numberOfPoints(), 2), N.Int16)
-        for b in universe.beadIterator():
-            bead_data[b.index, 0] = b.bead_number
-            bead_data[b.index, 1] = b.atom.numberOfBeads()
-        self.global_data.set('bead_data', bead_data)
-
+        if self.spring_parameters:
+            indices = [x[:2] for x in self.spring_parameters]
+            parameters = [x[2:] for x in self.spring_parameters]
+            terms.append(HarmonicDistanceTerm(universe._spec,
+                                              N.array(indices),
+                                              N.array(parameters),
+                                              "path integral spring"))
         terms.extend(self.ff.evaluatorTerms(self.universe,
-                                            subset1, subset2,
+                                            self.subset1, self.subset2,
                                             self.global_data))
 
         from MMTK_forcefield import Evaluator
         if threads is None:
             import MMTK.ForceFields
-            threads = MMTK.ForceFields.default_energy_threads;
-        self.evaluator = Evaluator(N.array(terms), 1./nbeads,
+            threads = MMTK.ForceFields.default_energy_threads
+        self.evaluator = Evaluator(N.array(terms),
+                                   1./self.global_data.get('nbeads'),
                                    threads, mpi_communicator)
 
     def checkUniverseVersion(self):
